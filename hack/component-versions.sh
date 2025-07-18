72b49617d6dcdfb46c029c7274986ba4100e4878
Knative Automation <automation@knative.team>
2025-07-18 01:51:38 +0000

    update components

--- hack/component-versions.sh
+++ hack/component-versions.sh
@@ -12,6 +12,6 @@ set_versions() {
 
 	# gets updated programatically via workflow -> PR creation
 	knative_serving_version="v1.18.1"
-	knative_eventing_version="v1.18.1"
+	knative_eventing_version="v1.18.2"
 	contour_version="v1.18.0"
 }
